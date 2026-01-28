2b30a7264e8de524708faa4b1248c716332186af
NathanDroz08 <nathan.droz2008@gmail.com>
2026-01-28 15:42:02 +0100

    push

---
+++ LaboratorioMAUI/AppConvertitore/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace AppConvertitore
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            //Inizializza i componenti grafici 
+            InitializeComponent();
+        }
+
+        private void btnConverti_Clicked(object sender, EventArgs e)
+        {
+            String valoreImporto = entConversione.Text;
+
+            double franchi = Convert.ToDouble(valoreImporto);
+
+            double euro = franchi * 1.07;
+
+
+        }
+    }
+
+}
